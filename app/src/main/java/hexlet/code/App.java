7765ee670f824e618972976a6b00e13e1d95a998
Lex200999 <lex200999@gmail.com>
2024-01-13 12:09:58 +0300

    fix failure

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,8 +4,9 @@ import static hexlet.code.Engine.algorithm;
 
 public class App {
     public static void main(String[] args) {
+        System.out.println("/May I have your name?/");
         System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet");
+        System.out.println("1 - Greet/");
         System.out.println("2 - Even");
         System.out.println("3 - Calc");
         System.out.println("4 - GCD");
